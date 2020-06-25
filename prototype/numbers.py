209e85c9fc4263f10f74cfe0c9151cd4abb2a0ff
showa_yojyo <yojyo@hotmail.com>
2020-06-25 13:00:12 +0900

    (prototype) Add numbers.py

---
+++ prototype/numbers.py
@@ -0,0 +1,236 @@
+"""
+PROTOTYPE
+"""
+
+from collections import Counter
+
+_bad_numbers = {-1, 0, 10, 11}
+
+def _bounds(nlower, nupper):
+    """
+    >>> _bounds(1, 2)
+    (1, 2)
+    >>> _bounds(2, 1)
+    (1, 2)
+    """
+
+    if nlower > nupper:
+        nlower, nupper = nupper, nlower
+    return (nlower, nupper)
+
+def clip(num):
+    """Clip a number to between 1 and 9
+
+    >>> clip(0)
+    1
+    >>> clip(10)
+    9
+    >>> clip(5)
+    5
+    """
+
+    return max(1, min(9, num))
+
+
+# Resolvers for tempai of four tiles
+
+
+def resolve_waiting_tiles_4(tiles):
+    """Resolve all the waiting tiles of four tiles.
+
+    ``tiles`` must be a sequence and ordered.
+
+    >>> resolve_waiting_tiles_4([5] * 4) == (1, {3, 4, 6, 7})
+    True
+    >>> resolve_waiting_tiles_4([1] * 4) == (1, {2, 3})
+    True
+    >>> resolve_waiting_tiles_4([9] * 4) == (1, {7, 8})
+    True
+
+    >>> resolve_waiting_tiles_4([4, 4, 4, 5]) == (0, {3, 6, 5})
+    True
+    >>> resolve_waiting_tiles_4([4, 5, 5, 5]) == (0, {3, 6, 4})
+    True
+    >>> resolve_waiting_tiles_4([1, 1, 1, 2]) == (0, {2, 3})
+    True
+    >>> resolve_waiting_tiles_4([1, 2, 2, 2]) == (0, {1, 3})
+    True
+    >>> resolve_waiting_tiles_4([8, 8, 8, 9]) == (0, {7, 9})
+    True
+    >>> resolve_waiting_tiles_4([8, 9, 9, 9]) == (0, {7, 8})
+    True
+
+    Two different pairs:
+
+    >>> resolve_waiting_tiles_4([1, 1, 2, 2]) == (0, {1, 2})
+    True
+
+    >>> resolve_waiting_tiles_4([1, 1, 2, 3]) == (0, {1, 4})
+    True
+    >>> resolve_waiting_tiles_4([1, 1, 5, 6]) == (0, {4, 7})
+    True
+    >>> resolve_waiting_tiles_4([1, 1, 5, 7]) == (0, {6})
+    True
+    >>> resolve_waiting_tiles_4([1, 1, 5, 9]) == (1, {3, 4, 5, 6, 7, 8, 9})
+    True
+    >>> resolve_waiting_tiles_4([1, 1, 8, 9]) == (0, {7})
+    True
+    >>> resolve_waiting_tiles_4([1, 2, 2, 3]) == (0, {2})
+    True
+    >>> resolve_waiting_tiles_4([1, 2, 3, 3]) == (0, {3})
+    True
+    """
+
+    assert len(tiles) == 4
+    bag = Counter(tiles)
+
+    if (ntype := len(bag)) == 1:
+        # Not a kong but a Pong and a single
+        return (1, _resolve_single_for_tower(tiles[0], False))
+    elif ntype == 2:
+        # On tempai
+
+        if set(bag.values()) == {3, 1}:
+            _, second = bag.most_common(ntype)
+            # case 1: A Pong and a single
+            waiting_tiles = {second[0]}
+            # case 2: Two pairs
+            waiting_tiles |= _resolve_waiting_for_chow(*bag.keys())
+        else:
+            # A double-Pong waiting
+            waiting_tiles = set(bag.keys())
+
+        return (0, waiting_tiles)
+    elif ntype == 3:
+        # AABC pattern
+        # Frequency: 2, 1, 1.
+        _, second1, second2 = bag.most_common(ntype)
+        nlower, nupper = _bounds(second1[0], second2[0])
+        tiles = _resolve_waiting_for_chow(nlower, nupper)
+        if tiles:
+            # Tempai
+            return (0, tiles)
+
+        # 1 shanten
+        waiting_tiles = _resolve_single_for_tower(nlower, True)
+        waiting_tiles |= _resolve_single_for_tower(nupper, True)
+        return (1, waiting_tiles)
+
+    # ABCD pattern
+    return _resolve_waiting_tiles_4_abcd(tiles)
+
+
+def _resolve_waiting_tiles_4_abcd(tiles):
+    """Resolve all the waiting tiles of ABCD pattern.
+
+    >>> resolve_waiting_tiles_4([1, 2, 3, 4]) == (0, {1, 4})
+    True
+    >>> resolve_waiting_tiles_4([3, 4, 5, 6]) == (0, {3, 6})
+    True
+    >>> resolve_waiting_tiles_4([6, 7, 8, 9]) == (0, {6, 9})
+    True
+    >>> resolve_waiting_tiles_4([1, 7, 8, 9]) == (0, {1})
+    True
+
+    >>> resolve_waiting_tiles_4([1, 4, 8, 9])
+    (1, {1, 4, 7})
+    >>> resolve_waiting_tiles_4([1, 2, 8, 9])
+    (1, {1, 2, 3, 7, 8, 9})
+    >>> resolve_waiting_tiles_4([1, 3, 5, 7])
+    (1, {1, 2, 3, 4, 5, 6, 7})
+    >>> resolve_waiting_tiles_4([2, 4, 6, 8])
+    (1, {2, 3, 4, 5, 6, 7, 8})
+    """
+
+    assert len(set(tiles)) == 4
+
+    waiting_tiles = set()
+    kaisa = [tiles[i + 1] - tiles[i] for i in range(0, 3)]
+
+    # Case of BCD is a Chow
+    if kaisa[1] == kaisa[2] == 1:
+        waiting_tiles.add(tiles[0])
+
+    # Case of ABC is a Chow
+    if kaisa[0] == kaisa[1] == 1:
+        waiting_tiles.add(tiles[-1])
+
+    if waiting_tiles:
+        # On tempai
+        return (0, waiting_tiles)
+
+    # Case of AB, BC, CD is a tower
+    # A tower and two isolated tiles
+    for i in range(3):
+        welcome = _resolve_waiting_for_chow(
+            tiles[i], tiles[i + 1])
+        if welcome:
+            waiting_tiles |= welcome
+            waiting_tiles.add(tiles[i - 1])
+            waiting_tiles.add(tiles[(i + 2) % 4])
+
+    return (1, waiting_tiles)
+
+
+def _resolve_single_for_tower(num, allow_pair=True):
+    """Return all the numbers that make a tower with the number ``num``.
+
+    >>> _resolve_single_for_tower(1) == {1, 2, 3}
+    True
+    >>> _resolve_single_for_tower(1, False) == {2, 3}
+    True
+    >>> _resolve_single_for_tower(5) == {3, 4, 5, 6, 7}
+    True
+    >>> _resolve_single_for_tower(5, False) == {3, 4, 6, 7}
+    True
+    >>> _resolve_single_for_tower(9) == {7, 8, 9}
+    True
+    >>> _resolve_single_for_tower(9, False) == {7, 8}
+    True
+    """
+
+    welcome_numbers = set(range(num - 2, num + 2 + 1))
+    welcome_numbers -= _bad_numbers
+    if not allow_pair:
+        welcome_numbers.remove(num)
+
+    return welcome_numbers
+
+
+def _resolve_single_for_pair(num):
+    """An unused function"""
+    return {num}
+
+
+def _resolve_waiting_for_chow(nlower, nupper):
+    """Return one or two tiles that make a Chow with ``tower``.
+
+    >>> _resolve_waiting_for_chow(1, 2) == {3}
+    True
+    >>> _resolve_waiting_for_chow(8, 9) == {7}
+    True
+    >>> _resolve_waiting_for_chow(1, 3) == {2}
+    True
+    >>> _resolve_waiting_for_chow(7, 9) == {8}
+    True
+    >>> _resolve_waiting_for_chow(5, 6) == {4, 7}
+    True
+    >>> not _resolve_waiting_for_chow(3, 3)
+    True
+    >>> not _resolve_waiting_for_chow(1, 9)
+    True
+    """
+
+    if nlower > nupper:
+        nlower, nupper = nupper, nlower
+
+    if (diff := nupper - nlower) == 1:
+        # A two-sided waiting
+        # An edge waiting
+        # A closed waiting
+        return {nlower - 1, nupper + 1} - _bad_numbers
+    elif diff == 2:
+        # A closed waiting
+        return {nlower + 1}
+    else:
+        return None
