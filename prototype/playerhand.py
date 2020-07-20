fc8965b65d9ccee99bf164c72c70a30cca2b36db
showa_yojyo <yojyo@hotmail.com>
2020-07-20 11:34:04 +0900

    (prototype) Add commit_chow and commit_pung

--- prototype/playerhand.py
+++ prototype/playerhand.py
@@ -3,8 +3,61 @@ mahjong.playerhand
 """
 
 from collections import Counter
+from enum import Enum
 
 import tiles
+
+class DiscardedBy(Enum):
+    """Position of the player that discarded the tile"""
+
+    LEFT = '上家'
+    CENTER = '対面'
+    RIGHT = '下家'
+
+
+class Meld:
+    """Base class"""
+
+    def __init__(self, tileinfo, concealed: bool, discarded_by: DiscardedBy):
+        self.tileinfo = tileinfo
+        self.concealed = concealed
+        self.discarded_by = discarded_by
+
+
+class Chow(Meld):
+    """Chow"""
+
+    def __init__(self, tileinfo, concealed: bool):
+        super().__init__(tileinfo, concealed, DiscardedBy.LEFT)
+
+    @property
+    def minipoints(self):
+        """Return the value of minipoints (fu)"""
+        return 0
+
+class Pung(Meld):
+    """Pung"""
+
+    _minipoints_base = 2
+
+    def __str__(self):
+        return f'{self.__class__.__name__}({self.tileinfo}, {self.concealed}, {self.discarded_by})'
+
+    @property
+    def minipoints(self):
+        """Return the value of minipoints (fu)"""
+        value = self._minipoints_base
+        if self.concealed:
+            value <<= 1
+        if not tiles.is_simple(self.tileinfo):
+            value <<= 1
+        return value
+
+
+class Kong(Pung):
+    """Kong"""
+
+    _minipoints_base = 8
 
 
 class PlayerHand:
@@ -76,19 +129,17 @@ class PlayerHand:
         if tile_class == tiles.TileClass.HONOR:
             return False
 
-        suit_tiles = self.get_concealed_part_by_class(tile_class)
-        numbers = tiles.convert_suit_to_number(suit_tiles)
-        number = tiles.get_suit_number(discarded_tile)
+        target_tiles = self.get_concealed_part_by_class(tile_class)
 
-        numm1 = number - 1 in numbers
-        nump1 = number + 1 in numbers
+        numm1 = discarded_tile - 1 in target_tiles
+        nump1 = discarded_tile + 1 in target_tiles
         if not numm1 and not nump1:
             return False
 
         if numm1:
-            return number - 2 in numbers or nump1
+            return discarded_tile - 2 in target_tiles or nump1
         if nump1:
-            return number + 2 in numbers
+            return discarded_tile + 2 in target_tiles
 
         return False
 
@@ -124,3 +175,54 @@ class PlayerHand:
 
 
     # TODO: 加槓
+
+
+    def commit_chow(
+            self,
+            new_tile: tiles.Tile,
+            tile1: tiles.Tile,
+            tile2: tiles.Tile):
+        """Add a Chow to the exposed part.
+
+        >>> player_hand = PlayerHand('12457789m45p346s')
+        >>> target_tile = tiles.tiles('8m')
+        >>> tile1, tile2 = tiles.tiles('7m'), tiles.tiles('9m')
+        >>> player_hand.commit_chow(target_tile, tile1, tile2)
+        >>> chow = player_hand.exposed_parts[0]
+        >>> isinstance(chow, Chow)
+        True
+        >>> chow.concealed
+        False
+        >>> print(chow.discarded_by)
+        DiscardedBy.LEFT
+        >>> player_hand.concealed_parts[tile1]
+        1
+        >>> player_hand.concealed_parts[target_tile]
+        1
+        >>> player_hand.concealed_parts[tile2]
+        0
+        """
+
+        self.exposed_parts.append(Chow([new_tile, tile1, tile2], False))
+        self.concealed_parts.subtract([tile1, tile2])
+
+
+    def commit_pung(self, tile: tiles.Tile, discarded_by: DiscardedBy):
+        """Add a Pung to the exposed part.
+
+        >>> player_hand = PlayerHand('2457789m248p14s白')
+        >>> target_tile = tiles.tiles('7m')
+        >>> player_hand.commit_pung(target_tile, DiscardedBy.CENTER)
+        >>> pung = player_hand.exposed_parts[0]
+        >>> assert isinstance(pung, Pung)
+        >>> assert pung.tileinfo == target_tile
+        >>> pung.concealed
+        False
+        >>> print(pung.discarded_by)
+        DiscardedBy.CENTER
+        >>> player_hand.concealed_parts[target_tile]
+        0
+        """
+
+        self.exposed_parts.append(Pung(tile, False, discarded_by))
+        self.concealed_parts.subtract({tile: 2})
